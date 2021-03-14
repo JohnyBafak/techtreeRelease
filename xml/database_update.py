3e625f45e8786477f3eb0b09f8a94ad003b9d9a2
JohnnyBafak <JohnyBafak@users.noreply.github.com>
2021-03-14 13:40:31 +0100

    update database system

---
+++ xml/database_update.py
@@ -0,0 +1,21 @@
+import zipfile, glob
+
+verF = 'version'
+
+# read current database
+with open(verF) as f:
+    line = f.read()
+    ver = int(line) if len(line) else 0
+    
+ver +=1
+print "> Updating layout database:", ver
+
+# write new database   
+with open(verF,'w') as f:
+    f.write( str(ver) )
+    
+# zip layouts
+with zipfile.ZipFile('xml.pkg', 'w') as zf:
+    zf.write(verF)
+    for f in glob.iglob('./*/*.xml'):
+        zf.write(f, f)   
